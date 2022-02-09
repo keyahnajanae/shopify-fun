13c491f29edcb791287c758997cf08b6a5821cb7
keyahnajanae <keyahna_blanford@hotmail.com>
2022-02-09 15:15:40 -0600

    working on navbar

---
+++ shopify-fun-1/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom'
+
+
+
+function Navbar() {
+  return (
+    <>
+
+    
+<nav className="navbar">
+    <div className="navbar-container">
+        <Link to path="/" className="navbar-logo">
+            DSNR <i className="fab fa-typo3"/>
+        </Link>
+    </div>
+</nav>
+    </>
+  );
+}
+
+export default Navbar;
