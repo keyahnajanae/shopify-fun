6debd5a7825fd1a2537ee93682b4104b4f222d11
keyahnajanae <keyahna_blanford@hotmail.com>
2022-02-09 17:34:58 -0600

    app and navbar css updated

--- shopify-fun-1/src/components/Navbar.js
+++ shopify-fun-1/src/components/Navbar.js
@@ -1,13 +1,27 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css';
 
 
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+       setButton(false);
+    } else{
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton)
+
 
   return (
     <>
@@ -29,9 +43,7 @@ function Navbar() {
           <li className='nav-item'>
             <Link to = '/services' className='nav-links' onClick={closeMobileMenu}>Services</Link>
           </li>
-          <li className='nav-item'>
-            <Link to = '/handbags' className='nav-links' onClick={closeMobileMenu}>Handbags</Link>
-          </li><li className='nav-item'>
+        <li className='nav-item'>
             <Link to = '/testimonials' className='nav-links' onClick={closeMobileMenu}>Testimonials</Link>
           </li>
           <li className='nav-item'>
@@ -42,6 +54,7 @@ function Navbar() {
             </Link>
           </li>
         </ul>
+        {button && <Button buttonStyle='btn--outline'> Signup</Button>}
     </div>
 </nav>
 
